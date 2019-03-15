3bd8a43a4d1fbb599f54d3980db044061e2aea6b
CharuTalesara <36394482+CharuTalesara@users.noreply.github.com>
2019-03-15 14:32:06 +0530

    Add files via upload

---
+++ Trees/NumberOfFullNodesInBT.java
@@ -0,0 +1,57 @@
+
+
+
+public class NumberOfFullNodesInBT
+{
+	public static void main(String args[])
+	{
+		BinaryTree bt=new BinaryTree();
+
+		bt.insert(1);
+		bt.insert(2);
+		bt.insert(3);
+		bt.insert(4);
+		bt.insert(5);
+		bt.insert(6);
+		bt.insert(7);
+		//bt.insert(8);
+		//bt.insert(9);
+		//bt.insert(10);
+		//bt.insert(11);
+		bt.preorder(bt.root);
+		System.out.println();
+
+		System.out.println("Full Nodes Count : "+fullNodesCount(bt.root));
+	}
+
+	public static int fullNodesCount(BinaryTreeNode root)
+	{
+		int count = 0;
+
+		FixedSizeCircularArrayQueue q=new FixedSizeCircularArrayQueue(20);
+		
+		q.enQueue(root);
+		
+			
+		while(!q.isEmpty())
+		{
+			BinaryTreeNode currnode=q.front();
+
+			if(currnode.left!=null && currnode.right!=null)
+			{
+				count++;
+				q.enQueue(currnode.left);
+				q.enQueue(currnode.right);				
+			}
+
+			else if(currnode.left != null)
+				q.enQueue(currnode.left);
+			else if(currnode.right!=null)
+				q.enQueue(currnode.right);
+			
+			q.dequeue();
+		}
+
+		return count;
+	}
+}
