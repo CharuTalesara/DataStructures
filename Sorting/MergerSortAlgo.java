45ddd5d18081b6182f1bfec515a9aa21b7b98971
CharuTalesara <36394482+CharuTalesara@users.noreply.github.com>
2019-04-18 12:23:32 +0530

    Add files via upload

--- Sorting/MergerSortAlgo.java
+++ Sorting/MergerSortAlgo.java
@@ -1,15 +1,19 @@
 
-
+//Including count variable to get the number of inversions
 
 public class MergerSortAlgo
 {
-
+	static int count=0;
 	public static void main(String args[])
 	{
-		int[] a={38,27,43,3,9,82,10};
+		//int[] a={38,27,43,3,9,82,10};
+
+		int[] a={2,3,8,6,1};
+		
 		print(a);				
 		mergesort(a,0,a.length-1);
 		print(a);
+		System.out.println(count);
 	}
 
 	public static void mergesort(int[] a,int left,int right)
@@ -28,6 +32,7 @@ public class MergerSortAlgo
 
 	public static void merge(int[] a,int left,int mid,int right)
 	{
+		
 		int n1=mid-left+1;
 		int n2=right-mid;
 
@@ -51,12 +56,15 @@ public class MergerSortAlgo
 				a[k]=l[i];
 				i++;
 				k++;
+				//count++;
+				
 			}
 			else
 			{
 				a[k]=r[j];
 				j++;
 				k++;
+				count++;
 			}
 		}
 
@@ -66,6 +74,7 @@ public class MergerSortAlgo
 			{
 				a[k]=l[i];
 				k++;
+				count++;
 			}
 		}
 		
@@ -78,6 +87,7 @@ public class MergerSortAlgo
 			}
 		}
 		//print(a);
+		
 		
 	}
 
