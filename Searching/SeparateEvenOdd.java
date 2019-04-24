79e0ed8c7da3f108d02406f73fca425e5773c009
CharuTalesara <36394482+CharuTalesara@users.noreply.github.com>
2019-04-24 12:02:38 +0530

    Add files via upload

---
+++ Searching/SeparateEvenOdd.java
@@ -0,0 +1,57 @@
+/*
+Separate even and odd numbers- Given an array A[], write a function that segregates even n odd. The function should put all even numbers first and then the odds..the order of the numbers is not relevant
+
+eg- i/p- 12 34 45 9 8 90 3
+o/p      12 34 90 8 9 45 3
+
+*/
+
+public class SeparateEvenOdd
+{
+	public static void main(String args[])
+	{
+		int[] a={12,34,45,9,8,90,3};
+		print(a);
+		segregate(a);
+		print(a);
+	}
+	
+	//O(n)
+
+	public static void segregate(int[] a)
+	{
+		int i=0;
+		
+		FixedSizeCircularArrayQueue q=new FixedSizeCircularArrayQueue();
+
+		while(i<a.length)
+		{
+			if(a[i]%2!=0)
+			{
+				q.enQueue(i);
+				
+			}
+
+			else
+			{
+				if(!q.isEmpty())
+				{
+					int j=q.dequeue();
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				
+				}
+				
+			}
+			i++;
+		}
+	}
+
+	public static void print(int[] a)
+	{
+		for(int i=0;i<a.length;i++)
+			System.out.print(a[i]+" ");
+		System.out.println();
+	}
+}
