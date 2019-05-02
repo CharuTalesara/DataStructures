4624ccbc872dd2520b5f51efb3f5451d3d7647f9
CharuTalesara <36394482+CharuTalesara@users.noreply.github.com>
2019-05-02 12:04:44 +0530

    Add files via upload

--- Queue/MaxSumInSlidingWindow.java
+++ Queue/MaxSumInSlidingWindow.java
@@ -4,31 +4,33 @@ public class MaxSumInSlidingWindow
 {
 	public static void main(String args[])
 	{
-		int[] a={1,3,-1,-3,5,3,6,7};
+		int[] a={100,3,-1,-3,5,3,6,7};
 				
 		
 		maxInSlidingWindow(a,3);
 	}
 
+
 	public static void maxInSlidingWindow(int[] a,int w)
 	{
 		FixedSizeCircularArrayQueue q=new FixedSizeCircularArrayQueue(w);
-		int max=a[0];
+		int max=-10000;
 
-		for(int i=0;i<w;i++)
+		for(int i=0;i<a.length;i++)
 		{
-			q.enQueue(a[i]);
-			if(max<a[i])
-				max=a[i];		
-		}		
-		q.printQ();
-		System.out.println("Max of Q : "+max);
-		
-		for(int i=w;i<a.length;i++)
-		{
-			q.dequeue();
-			q.enQueue(a[i]);
+			if(i<w)
+				q.enQueue(a[i]);
+			else
+			{
+				
+				int deletedele=q.dequeue();
+				if(max==deletedele)
+					max=q.frontele();
+				q.enQueue(a[i]);
+				
+			}
 			q.printQ();
+			
 			if(max<a[i])
 				max=a[i];
 			System.out.println("Max of Q : "+max);
