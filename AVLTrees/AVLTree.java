9d66b0b6571de04eede106d6e04b7f4ca06f7da1
CharuTalesara <36394482+CharuTalesara@users.noreply.github.com>
2019-05-17 16:07:46 +0530

    Add files via upload

--- AVLTrees/AVLTree.java
+++ AVLTrees/AVLTree.java
@@ -19,7 +19,7 @@ public class  AVLTree
 
 	public AVLTreeNode insert(AVLTreeNode root,AVLTreeNode parent,int data)
 	{
-		/* the parent parameter is passed to check is tree is empty so that we can set Tree's root as this.root */
+		/* the parent parameter is passed to check if tree is empty so that we can set Tree's root as this.root */
 		if(parent==null)
 		{
 			this.root=new AVLTreeNode(data);
@@ -43,21 +43,80 @@ public class  AVLTree
 		
 		if(balance>1)
 		{
-			if(root.left.data>data )
+			if(root.left.data>data ) //CASE 1
 			{
 				return SingleRotateLeft(root);	
 			}
 			else
-				return DoubleRotateWithLeft(root);
+				return DoubleRotateWithLeft(root); //CASE 2
 		}
 		if(balance<-1)
 		{
 			if(root.right.data<data)
-				return SingleRotateRight(root);
+				return SingleRotateRight(root); //CASE 4
 			else
-				return DoubleRotateWithRight(root);
+				return DoubleRotateWithRight(root); //CASE 3
 		}
 			
+		return root;
+	}
+
+	public AVLTreeNode delete(AVLTreeNode root,int key)
+	{
+		if(root == null)
+			return root;
+				
+		else if(root.data>key)
+			root.left= delete(root.left,key);
+
+		else if(root.data<key)
+			root.right= delete(root.right,key);
+
+		else	// Got the node to be deleted
+		{
+			if(root.left==null && root.right==null)
+				root=null;
+			else if(root.left==null)
+				root=root.right;
+			else if(root.right==null)
+				root=root.left;
+
+			else
+			{
+				AVLTreeNode temp=getSuccessor(root);
+				root.data=temp.data;
+				root.right=delete(root.right,temp.data);
+			}
+		}
+
+		if(root==null)
+			return root;
+	
+		root.setHeight(Math.max(Height(root.left),Height(root.right))+1);
+
+		int balance =getBalance(root);
+		
+		if(balance>1  && getBalance(root.left)>=0)
+		{
+			return SingleRotateLeft(root);
+		}
+
+		if(balance>1  && getBalance(root.left)<0)
+		{
+			return DoubleRotateWithLeft(root);
+		}
+
+		if(balance<-1  && getBalance(root.right)>=0)
+		{
+			return DoubleRotateWithLeft(root);
+		}
+
+		if(balance<-1 && getBalance(root.right)<0)
+		{
+			return SingleRotateLeft(root);
+		}
+
+
 		return root;
 	}
 
@@ -68,33 +127,14 @@ public class  AVLTree
 		return Height(x.left)-Height(x.right);
 	}
 
-	public AVLTreeNode getParent(AVLTreeNode x)
-	{
-		AVLTreeNode curr=this.root;
-		
-		if(curr==null)
-			return null;
-
-		AVLTreeNode parent=null;
-		
-		while(curr!=null && curr.data!=x.data)
-		{
-			parent=curr;
-			if(curr.data>x.data)
-				curr=curr.left;
-			else
-				curr=curr.right;
-		}
-
-		return parent;
-	}
-
 	public AVLTreeNode SingleRotateLeft(AVLTreeNode x)
 	{
 		AVLTreeNode z=getParent(x);
 		AVLTreeNode w=x.left;
 		x.left=w.right;
 		w.right=x;
+
+		// Setting x's parent's child as w .. n if not tree's root is  w
 
 		if(z!=null )
 			if(z.right==x)
@@ -134,15 +174,18 @@ public class  AVLTree
 
 	public AVLTreeNode DoubleRotateWithLeft(AVLTreeNode z)
 	{
+		// converting in CASE 1
 		z.setLeft(SingleRotateRight(z.left));
 		return SingleRotateLeft(z);
 	}
 	
 	public AVLTreeNode DoubleRotateWithRight(AVLTreeNode z)
 	{
+		// Converting in case 4
 		z.setRight(SingleRotateLeft(z.right));
 		return SingleRotateRight(z);
 	}
+
 	
 	public int Height(AVLTreeNode root)
 	{
@@ -150,6 +193,97 @@ public class  AVLTree
 			return 0;
 		else
 			return root.getHeight();
+	}
+
+	public AVLTreeNode getParent(AVLTreeNode x)
+	{
+		AVLTreeNode curr=this.root;
+		
+		if(curr==null)
+			return null;
+
+		AVLTreeNode parent=null;
+		
+		while(curr!=null && curr.data!=x.data)
+		{
+			parent=curr;
+			if(curr.data>x.data)
+				curr=curr.left;
+			else
+				curr=curr.right;
+		}
+
+		return parent;
+	}
+
+	public AVLTreeNode getMin(AVLTreeNode x)	
+	{
+		if(x.left==null)
+			return x;
+
+		return getMin(x.left);
+	}
+
+	public AVLTreeNode getMax(AVLTreeNode x)	
+	{
+		if(x.right==null)
+			return x;
+
+		return getMax(x.right);
+	}
+
+	public AVLTreeNode getSuccessor(AVLTreeNode x)
+	{
+
+		AVLTreeNode currnode = search(x.data);
+
+		if(currnode ==null)
+			return null;		
+		
+		if(currnode.right!=null)
+			return getMin(currnode.right);
+		
+		else
+		{
+			AVLTreeNode y=root;
+			AVLTreeNode successor = null;
+
+			while(x.data!=y.data)
+			{
+				if(y.data>x.data)
+				{
+					successor=y;
+					y=y.left;
+				}
+				else
+					y=y.right;
+			}
+
+			return successor;
+		}
+		
+	}
+
+
+	public AVLTreeNode search(int x)
+	{
+		AVLTreeNode currnode=root;
+
+		if(currnode==null)
+			return null;
+		
+		if(currnode.data==x)
+			return  currnode;
+
+		while(currnode!=null && currnode.data!=x)
+		{
+			if(currnode.data<x)
+				currnode=currnode.right;
+			else
+				currnode=currnode.left;
+		}
+
+		return currnode;
 	}
 
 	public void printTree(AVLTreeNode root)
--- AVLTrees/AVLTreeDemo.java
+++ AVLTrees/AVLTreeDemo.java
@@ -40,9 +40,9 @@ public class AVLTreeDemo
 
 		// FOR CASE 3 
 	
-		t.insert(t.root,t.root,10);
+		/*t.insert(t.root,t.root,10);
 		t.insert(t.root,t.root,15);
-		t.insert(t.root,t.root,12);
+		t.insert(t.root,t.root,12);*/
 		/*t.insert(t.root,t.root,7);
 		t.insert(t.root,t.root,8);*/
 
@@ -51,8 +51,55 @@ public class AVLTreeDemo
 		t.insert(t.root,t.root,15);
 		t.insert(t.root,t.root,20);
 		t.insert(t.root,t.root,12);*/
-		System.out.println("*****Prinintg AVL Tree***********");
+		/*System.out.println("*****Prinintg AVL Tree***********");
 		t.printTree(t.root);
 		System.out.println("*****Prinintg AVL Tree***********");
+		System.out.println("******Search Checking******");*/
+		//System.out.println(t.search(12).data);
+
+		// Evaluating different cases for deletion 
+
+		t.insert(t.root,t.root,14);
+		t.insert(t.root,t.root,21);
+		t.insert(t.root,t.root,15);
+		t.insert(t.root,t.root,28);
+		t.insert(t.root,t.root,26);
+		t.insert(t.root,t.root,35);
+		//t.insert(t.root,t.root,15);
+		t.insert(t.root,t.root,4);
+		t.insert(t.root,t.root,2);
+		t.insert(t.root,t.root,9);
+		t.insert(t.root,t.root,7);
+		t.insert(t.root,t.root,10);
+		/*t.insert(t.root,t.root,7);
+		t.insert(t.root,t.root,10);*/
+
+		//t.printTree(t.root);
+		/*System.out.println("Minimum : "+t.getMin(t.root).data);
+		System.out.println("Maximum : "+t.getMax(t.root).data);
+		System.out.println("Suceesor of 26 : "+t.getSuccessor(t.search(26)).data);
+		System.out.println("Suceesor of 2 : "+t.getSuccessor(t.search(2)).data);*/
+
+		/*t.delete(t.root,15);
+		t.delete(t.root,21);
+		//t.delete(t.root,9);
+		System.out.println("*****Prinintg AVL Tree***********");
+		t.printTree(t.root);*/
+
+		t.insert(t.root,t.root,1);
+		System.out.println("*****Prinintg AVL Tree***********");
+		t.printTree(t.root);
+		t.delete(t.root,9);
+		t.delete(t.root,7);
+		t.delete(t.root,10);
+		System.out.println("*****Prinintg AVL Tree***********");
+		t.printTree(t.root);
+		/*t.delete(t.root,26);
+		System.out.println("*****Prinintg AVL Tree***********");
+		t.printTree(t.root);
+
+		t.delete(t.root,14);
+		System.out.println("*****Prinintg AVL Tree***********");
+		t.printTree(t.root);*/		
 	}
 }
