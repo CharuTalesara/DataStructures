2b0f594bd9f4f63bd088f274e817a906f786e84d
CharuTalesara <36394482+CharuTalesara@users.noreply.github.com>
2019-07-02 12:50:19 +0530

    Add files via upload

--- LinkedList/InsertionSortLL.java
+++ LinkedList/InsertionSortLL.java
@@ -13,17 +13,74 @@ public class InsertionSortLL
 		sll.insert(2,2);
 		sll.insert(5,3);
 		sll.insert(4,4);
-		sll.insert(3,5);
+		sll.insert(6,5);
 		sll.insert(7,6);
-		sll.insert(6,7);
+		sll.insert(3,7);
 		sll.insert(9,8);
 		sll.insert(8,9);
 		sll.insert(11,10);
 
 		sll.print();
-		sortLL(sll.head);
+		sortLL_1(sll.head);
 		sll.print();
 	}
+
+	public static ListNode sortLL_1(ListNode head)
+	{
+		if(head==null || head.next==null)
+			return head;
+
+		ListNode currnode=head.next;
+		ListNode currprev=head;
+	
+		while(currnode!=null)
+		{	
+			if(currnode.data>currprev.data)
+			{
+				currprev=currnode;
+				currnode=currnode.next;
+			}
+
+			else
+			{
+				ListNode tempcurr=head;
+				ListNode tempprev=head;
+
+				while(tempcurr.data<currnode.data)
+				{
+					tempprev=tempcurr;
+					tempcurr=tempcurr.next;
+				}
+				
+				/*ListNode temp1=currnode.next;
+
+				currnode.next=tempcurr;
+				tempcurr.next=temp1;
+				tempprev.next=currnode;
+
+				currprev=tempcurr;
+				currnode=temp1;*/
+				
+				currprev.next=currnode.next;
+				tempprev.next=currnode;
+				currnode.next=tempcurr;
+				currnode=currprev.next;
+
+
+				if(tempcurr==head)
+				{
+					currnode.next=head.next;
+					head=currnode;
+				}
+									
+			}	
+		}
+
+		
+		return head;
+	}
+			
+
 
 	public static ListNode sortLL(ListNode head)
 	{
