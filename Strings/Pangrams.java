b4b2988130d341470606eb739b9207377cf88eed
CharuTalesara <36394482+CharuTalesara@users.noreply.github.com>
2019-08-06 15:29:07 +0530

    Add files via upload

--- Strings/Pangrams.java
+++ Strings/Pangrams.java
@@ -5,8 +5,7 @@ public class Pangrams
 	public static void main(String args[])
 	{
 		String s="we promptly judged antique ivory buckles for the next prize";
-		
-		//System.out.println(Character.getNumericValue('a'));
+		 
 		System.out.println(isPangram(s));
 	}
 	
@@ -18,27 +17,14 @@ public class Pangrams
 		{
 			if(Character.isAlphabetic(s.charAt(i)))
 			{
-//				if(Character.isUpperCase(s.charAt(i)))
-//				{
-//					int n=Character.getNumericValue(s.charAt(i));
-//					System.out.println(n);
-//					n=n-65;
-//					a[n]=1;
-//				}
-//				else
-//				{
 					int n=Character.getNumericValue(s.charAt(i));
-					System.out.println("***");
-					System.out.println(s.charAt(i)+" "+n);
 					n=n-10;
 					a[n]=1;
-				//}
 			}
 		}
 		
 		for(int i=0;i<a.length;i++)
 		{
-			//System.out.print(a[i]+" ");
 			if(a[i]==0)
 				return "not pangram";
 		}
